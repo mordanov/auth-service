88638ddef0b41e7bae56ed09a6d529c8fc638776
Aleksandr Mordanov <aleksandr.mordanov.external@zalando.de>
2026-05-19 15:26:19 +0200

    conftest added

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -5,5 +5,7 @@ import pytest
 @pytest.fixture(scope="session")
 def event_loop():
     loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
     yield loop
     loop.close()
+    asyncio.set_event_loop(None)
